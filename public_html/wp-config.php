d76315f753c155bc3fd85b1e69a30b9bbec5fe89
Umairah <umairahmymy@gmail.com>
2014-05-08 13:35:21 +0800

    wp-config file changes
    wp-configv1.1

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'adminjCt54kv');
 define('DB_PASSWORD', '2gm6Tzg1Ysgl');
 
 /** MySQL hostname */
-define('DB_HOST', 'https://myralora-umairahmymy.rhcloud.com/phpmyadmin/');
+define('DB_HOST', 'https://myralora-umairahmymy.rhcloud.com/');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
