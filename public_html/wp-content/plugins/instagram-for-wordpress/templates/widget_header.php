39862c6bb71e6f5cb6232f2a583d0d1f03292677
Umairah <umairahmymy@gmail.com>
2014-05-08 15:49:23 +0800

    rearrangement of files
    rearrangement and removing public_html files

--- public_html/wp-content/plugins/instagram-for-wordpress/templates/widget_header.php
+++
@@ -1,9 +0,0 @@
-<h3 class="widget-title">
-  <?php
-    if ($settings['title']) {
-      print $settings['title'];
-    } else {
-      print "My Instagrams";
-    }
-  ?>
-</h3>
--- public_html/wp-content/themes/theron-lite/sidebar.php
+++
@@ -1,6 +0,0 @@
-<div id="sidebar">
-    <div class="widgets">          
-            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Right Sidebar') ) : ?> 
-            <?php endif; ?>
-            </div>
-    </div>
