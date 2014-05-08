3abcd05b2c1af78861442ca5f749082a79be2d5e
Umairah <umairahmymy@gmail.com>
2014-05-08 15:45:08 +0800

    Rearrangement of files

---
+++ wp-content/themes/photogram/footer.php
@@ -0,0 +1,11 @@
+
+    <footer class="footer row">
+      <?php colabs_credit(); ?>
+    </footer>
+  </div><!-- .row -->
+</div><!-- .main-container -->
+
+<?php wp_footer(); ?> 
+
+</body>
+</html>
